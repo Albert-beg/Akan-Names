e2f8e5fb658ef67295b1861b228524fdb27a2648
Albert Obwoge <albertnyambegaa@gmail.com>
2020-05-10 12:38:40 +0300

    add function

--- Js/index.js
+++ Js/index.js
@@ -1,1 +1,25 @@
-var
+function dayOfWeek(dayname){
+    switch(dayname){
+        case "Sunday":
+            return "Kwasi";
+        case "Monday":
+            return "Kwadwo";
+        case "Teusday":
+            return "Kwabena";
+        case "Wednesday":
+            return "Kwaku";
+        case "Thursday":
+            return "Yaw";
+        case "Friday":
+            return "Kofi";
+        case "Saturday":
+            return"Kwame";
+        default:
+                throw "incorrect day representation";
+    }
+    function testdayOfweek(){
+        var dayName =dayOfWeek();
+        alert(dayOfWeek);
+    }
+    var
+}
